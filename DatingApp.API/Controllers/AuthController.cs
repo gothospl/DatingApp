f7802978612bd8945aa80e8230bdeff103f3366e
Bartek <dziuba.bartosz@gmail.com>
2020-07-14 14:26:08 +0200

    progress for registration

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.UserName))
+            {
+                return BadRequest("Username already taken");
+            }
+
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDto.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
