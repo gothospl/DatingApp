25e96489addea43c1a1b1619f50ad000913034bd
Bartek <dziuba.bartosz@gmail.com>
2020-07-27 12:42:39 +0200

    adding alertifyjs

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 
 constructor(private http: HttpClient) { }
 
@@ -19,6 +21,11 @@ constructor(private http: HttpClient) { }
         }
       })
     );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 
   register(model: any) {
